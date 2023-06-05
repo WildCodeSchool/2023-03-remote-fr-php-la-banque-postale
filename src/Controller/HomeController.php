f3e0db5e6532e33c692669360931bd43edc3985c
Nina <nina.schadel@gmail.com>
2023-06-05 17:20:32 +0200

    début de la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,14 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'website' => 'LIGNE BLEUE',
+            'slogan' => '“Rendre visible les invisibles du numérique”',
+            'presentation' => '“La Ligne Bleue est un site dédié à accompagner les personnes qui rencontrent
+            des difficultés avec les nouvelles technologies. Il offre une variété de tutoriels pour aider les 
+            utilisateurs à maîtriser différents outils et applications. En créant un compte, les utilisateurs 
+            peuvent suivre leur progression et accéder à des formations personnalisées, leur permettant ainsi 
+            de développer leurs compétences technologiques à leur propre rythme.”',
+        ]);
     }
 }
