bcc5ecd4d5efa7a6b1b032ae1fd441e807e6f88c
titouwork <titouan.pro.lahely@gmail.com>
2023-06-06 17:31:53 +0200

    création de l entité category/controller/fixtures/repoitory et template

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+
+
+#[Route('/category')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+}
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{   
+    public const CATEGORIES = [
+        'Ligne bleue',
+        'Utiliser mon téléphone',
+        'Me divertir avec mon téléphone',
+        'Mail',
+        'Web',
+        'Sécurité',
+        'Communication',
+        'Me déplacer',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CATEGORIES as $categoryTitle) {
+            $category = new Category();
+            $category->setTitle($categoryTitle);
+            $manager->persist($category);
+            $this->addReference('category_' . $categoryTitle, $category);
+        }
+        $manager->flush();
+    }
+}
