d2422eab79fc4ad5f6442dd511de6f01e7f07277
Nina <nina.schadel@gmail.com>
2023-07-03 14:04:04 +0200

    favoris ajoutés, le back fonctionne bien

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
