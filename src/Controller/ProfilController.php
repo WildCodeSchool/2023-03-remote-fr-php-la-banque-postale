c457503fc716bfd3871636a61c4c1c9ca6cd3e34
Nina <nina.schadel@gmail.com>
2023-07-07 12:19:08 +0200

    les favoris sont mis dans une div dans la page profil, j'ai bien refait le front de cette page en même temps

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -11,8 +11,12 @@ class ProfilController extends AbstractController
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        $favoris = $user->getTutorialsBookmarked();
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+            'user' => $user,
+            'favoris' => $favoris, // Passer les tutoriels favoris à la vue
         ]);
     }
 }
