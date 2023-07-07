44600a7224478ef137c583a67aa2cc52e200e25e
titouwork <titouan.pro.lahely@gmail.com>
2023-07-07 16:10:04 +0200

    correction grump

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProgressRepository;
+use App\Repository\QuestionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(QuestionRepository $questionRepository, ProgressRepository $progressRepository): Response
     {
+        $questions = $questionRepository->findAll();
+        $progress = $progressRepository->findAll();
+
+        $score = 0;
+        $totalQuestions = count($questions);
+
+        foreach ($progress as $answer) {
+            $score += $answer->getScore();
+        }
+
+        $calculPercent = $totalQuestions > 0 ? ($score / $totalQuestions) * 100 : 0;
+
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+            'calculPercent' => $calculPercent,
         ]);
     }
 }
