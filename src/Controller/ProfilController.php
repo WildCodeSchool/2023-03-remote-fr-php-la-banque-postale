4999a6a40f057b1f1f9bc3f3e75404c099b0b508
titouwork <titouan.pro.lahely@gmail.com>
2023-07-10 12:20:37 +0200

    integration du % dans le profilController

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Services\PercenTool;
+use App\Repository\CategoryRepository;
 use App\Repository\ProgressRepository;
 use App\Repository\QuestionRepository;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,10 +14,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(QuestionRepository $questionRepository, ProgressRepository $progressRepository): Response
-    {
+    public function index(
+        QuestionRepository $questionRepository,
+        ProgressRepository $progressRepository,
+        CategoryRepository $categoryRepository,
+        PercenTool $percenTool
+    ): Response {
+        $user = $this->getUser();
+        $questions = $questionRepository->findAll();
+        $progress = $progressRepository->findBy(['user' => $user]);
+        $categories = $categoryRepository->findAll();
+        $success = [];
+        $categoryName = [];
+
+        foreach ($categories as $categoryId) {
+            $categoryObject = $categoryRepository->findOneBy(['id' => $categoryId]);
+            $title = $categoryId->getTitle();
+            $percentage = $percenTool->calculatePercentage($categoryObject);
+
+            $success[$categoryId->getId()] = $percentage;
+            $categoryName[$categoryId->getId()] = $title;
+        }
+
+        $score = 0;
+        $totalQuestions = count($questions);
+
+        foreach ($progress as $quizScore) {
+            $score += $quizScore->getScore();
+        }
+
+        $calculPercent = $totalQuestions > 0 ? ($score / $totalQuestions) * 100 : 0;
+
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
+            'calculPercent' => $calculPercent,
+            'success' => $success,
+            'categoryName' => $categoryName,
         ]);
     }
 }
