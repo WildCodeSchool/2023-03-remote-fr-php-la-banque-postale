32f001dd9a4dd8722e0214c38b14106309f39bdb
AMichelas <120266869+AMichelas@users.noreply.github.com>
2023-07-17 10:48:04 +0200

    work conflits

--- src/Form/ChoiceFriendType.php
+++ src/Form/ChoiceFriendType.php
@@ -6,20 +6,24 @@ use App\Entity\AddFriend;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ChoiceFriendType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('accept', CheckboxType::class, [
-                'label' => 'Accepter',
-                'required' => false,
-            ])
-            ->add('decline', CheckboxType::class, [
-                'label' => 'Refuser',
-                'required' => false,
+            ->add('choice', ChoiceType::class, [
+                'label' => false,
+                'choices' => [
+                    'Accepter' => 'accept',
+                    'Refuser' => 'decline',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'choice_attr' => [
+                    'class' => 'd-flex justify-content-between', // Ajoutez une classe Bootstrap pour la marge inférieure
+                ],
             ]);
     }
 
