b7a913bd838a829fb41f518fd8e486077de04f4a
Nina <nina.schadel@gmail.com>
2023-07-24 17:02:44 +0200

    video ajouté en supprimant le fichier migration et en le regenerant

--- migrations/Version20230724150133.php
+++ migrations/Version20230724150133.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230724134719 extends AbstractMigration
+final class Version20230724150133 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230724134719 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tutorial CHANGE video video VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE tutorial ADD video VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tutorial CHANGE video video LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE tutorial DROP video');
     }
 }
